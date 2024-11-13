a6486ad430936b4313d66346f500cd080f546489
unknown <alexandrovaa2003@gmail.com>
2024-11-13 23:04:23 +0300

    fix chiper

--- src/main/java/edu/program/encryptionprogram/VigenereCipher.java
+++ src/main/java/edu/program/encryptionprogram/VigenereCipher.java
@@ -2,19 +2,22 @@ package edu.program.encryptionprogram;
 
 public class VigenereCipher {
     private static final int ALPHABET_SIZE = 26; // Размер английского алфавита
-    private static final char BASE_CHAR = 'a';   // Начальный символ для сдвигов
+    private static final char BASE_CHAR = 'A';   // Начальный символ для сдвигов
 
     // Метод для шифрования текста
     public static String encrypt(String text, String key) {
-        String sanitizedText = text.replaceAll("[^a-zA-Z ]", "").toLowerCase();
+        // Приводим текст к верхнему регистру и удаляем все неалфавитные символы
+        String sanitizedText = text.replaceAll("[^A-Za-z]", "").toUpperCase();
+        key = key.toUpperCase();
         StringBuilder encryptedText = new StringBuilder();
-        String extendedKey = extendKey(sanitizedText, key);  // Расширяем ключ
 
+        // Шифрование
         for (int i = 0; i < sanitizedText.length(); i++) {
             char textChar = sanitizedText.charAt(i);
-            char keyChar = extendedKey.charAt(i);
+            char keyChar = key.charAt(i % key.length()); // Используем ключ циклично
+
             if (Character.isLetter(textChar)) {
-                int encryptedChar = (textChar - BASE_CHAR + keyChar - BASE_CHAR) % ALPHABET_SIZE;
+                int encryptedChar = (textChar - BASE_CHAR + (keyChar - BASE_CHAR)) % ALPHABET_SIZE;
                 encryptedText.append((char) (encryptedChar + BASE_CHAR));
             } else {
                 encryptedText.append(textChar);
@@ -26,11 +29,13 @@ public class VigenereCipher {
     // Метод для дешифрования текста
     public static String decrypt(String encryptedText, String key) {
         StringBuilder decryptedText = new StringBuilder();
-        String extendedKey = extendKey(encryptedText, key);
+        key = key.toUpperCase();
 
+        // Дешифрование
         for (int i = 0; i < encryptedText.length(); i++) {
-            char encryptedChar = Character.toLowerCase(encryptedText.charAt(i));
-            char keyChar = Character.toLowerCase(extendedKey.charAt(i));
+            char encryptedChar = encryptedText.charAt(i);
+            char keyChar = key.charAt(i % key.length()); // Используем ключ циклично
+
             if (Character.isLetter(encryptedChar)) {
                 int decryptedChar = (encryptedChar - keyChar + ALPHABET_SIZE) % ALPHABET_SIZE;
                 decryptedText.append((char) (decryptedChar + BASE_CHAR));
@@ -40,14 +45,4 @@ public class VigenereCipher {
         }
         return decryptedText.toString();
     }
-
-    // Вспомогательный метод для повторения ключа до нужной длины
-    private static String extendKey(String text, String key) {
-        StringBuilder extendedKey = new StringBuilder();
-        for (int i = 0; i < text.length(); i++) {
-            extendedKey.append(key.charAt(i % key.length()));
-        }
-        return extendedKey.toString();
-    }
-
 }
